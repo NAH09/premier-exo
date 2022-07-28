6492c6f9bb474db99cea8c7e305be2693744673c
NAH09 <manahari@quantifyafrica.com>
2022-07-28 16:45:51 +0000

    brouillon

---
+++ ope.py
@@ -0,0 +1,13 @@
+from re import A
+
+
+nombre= []
+
+class Addition : 
+    def __init__(self, nombre) :
+        self.nombre= nombre
+
+    def check (self)
+        self.vald=True
+        for s in self.nombre :
+            if not isinstance(nombre, (int, float)) :
