ddce5587678dc31148729a124d2c675ba81b61c6
NAH09 <manahari@quantifyafrica.com>
2022-07-29 09:08:39 +0000

    Ajout de la propriété product

--- ope.py
+++ ope.py
@@ -1,13 +1,23 @@
-from re import A
-
-
 nombre= []
 
 class Addition : 
     def __init__(self, nombre) :
         self.nombre= nombre
 
-    def check (self)
+    def check (self) :
         self.vald=True
+        print ('validé')
         for s in self.nombre :
-            if not isinstance(nombre, (int, float)) :
+            if not isinstance(s, (int)) :
+                self.vald =False
+                raise ValueError('entrez un entier')
+
+    def product (self):
+        self.prod=1
+        for s in self.nombre:
+            self.prod *= s
+        return self.prod
+
+ir= Addition([8,8,8])
+ir.check()
+ir.product()
